cac81ad5af0a660e0372ab93ff3d8133181a4f92
skypse <skypse0136@gmail.com>
2024-04-28 23:43:21 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Evento ao primeiro elemento com a classe "fab fa-youtube"
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", () => {
+  // Abre uma nova janela do navegador com o link para o vídeo do YouTube
+  window.open("https://www.youtube.com/watch?v=rBrd_3VMC3c");
+});
+
+// Evento ao segundo elemento com a classe "fab fa-youtube"
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", () => {
+  // Abre uma nova janela do navegador com o link para o vídeo do YouTube
+  window.open("https://www.youtube.com/watch?v=zqNTltOGh5c");
+});
+
+// Evento ao terceiro elemento com a classe "fab fa-youtube"
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", () => {
+  // Abre uma nova janela do navegador com o link para o vídeo do YouTube
+  window.open("https://www.youtube.com/watch?v=cb2w2m1JmCY");
+});
+
+// Evento ao quarto elemento com a classe "fab fa-youtube"
+document.getElementsByClassName("fab fa-youtube")[3].addEventListener("click", () => {
+  // Abre uma nova janela do navegador com o link para o primeiro artigo
+  window.open("https://www.youtube.com/watch?v=y4bZu56EylA");
+});
+
+// Evento ao quinto elemento com a classe "fab fa-youtube"
+document.getElementsByClassName("fab fa-youtube")[4].addEventListener("click", () => {
+  // Abre uma nova janela do navegador com o link para o segundo artigo
+  window.open("https://www.youtube.com/watch?v=r594pxUjcz4");
+});
+
+// Evento ao sexto elemento com a classe "fab fa-youtube"
+document.getElementsByClassName("fab fa-youtube")[5].addEventListener("click", () => {
+  // Abre uma nova janela do navegador com o link para o terceiro artigo
+  window.open("https://www.youtube.com/watch?v=u2bigf337aU");
+});
